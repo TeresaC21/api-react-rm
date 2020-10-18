5ee71779ebb1c7c110c88df1ae69e3cbb4b5ee53
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-10-18 10:54:58 -0500

    Updates

---
+++ precache-manifest.b701923884b001068b2929ee51fc6dc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7093a31951a5b414e17fada7f59a1955",
+    "url": "/api-react-rm/index.html"
+  },
+  {
+    "revision": "a3a3717e9bf37de3f1b6",
+    "url": "/api-react-rm/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "21185ee8a382519a16c1",
+    "url": "/api-react-rm/static/js/2.6b4cd1a4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/api-react-rm/static/js/2.6b4cd1a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3a3717e9bf37de3f1b6",
+    "url": "/api-react-rm/static/js/main.b25bf82b.chunk.js"
+  },
+  {
+    "revision": "b2f89d248807ca82be4e",
+    "url": "/api-react-rm/static/js/runtime-main.46e8bac6.js"
+  }
+]);
